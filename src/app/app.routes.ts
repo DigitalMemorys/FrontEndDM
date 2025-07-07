09bf4be823aff6a05ff648c1ae50285bdc1c256e
Alessandra Becerra <159406731+aleeBecerra@users.noreply.github.com>
2025-07-06 21:58:10 -0500

    fix: fixing routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { PageNotFoundComponent } from "./public/pages/page-not-found/page-not-found.component";
 import { HomeComponent } from './public/pages/home/home.component';
 const ProductManageComponent = () => import('./productmanage/pages/productmanage.component').then(m => m.ProductmanageComponent);
-const ShoppingCartPayments = () => import('./shoppingcartpayments/pages/shoppingcartpayments.components').then(m => m.ShowOrderInfoComponent);
+const ShoppingCartPayments = () => import('./shoppingcartpayments/pages/shoppingcartpayments.components').then(m => m.ShoppingcartpaymentsComponent);
 const GlobalChatInteraction = () => import('./globalchatinteraction/pages/global-chat-interaction.component').then(m => m.GlobalChatInteractionComponent);
 
 export const routes: Routes = [
